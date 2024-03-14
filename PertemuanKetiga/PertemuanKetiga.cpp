d9773ebe00cf160308954c5e18147d462b42be1d
akmaalsyh <taufansyah50@gmail.com>
2024-03-14 15:43:25 +0700

    1. looping dengan 1 ddimulai dari 1 hingga n-1

--- PertemuanKetiga/PertemuanKetiga.cpp
+++ PertemuanKetiga/PertemuanKetiga.cpp
@@ -1,15 +1,66 @@
 #include <iostream>
 using namespace std;
 
-void insertionsort() {
-    int i, j, n, temp;
-    int arr[20];
+void input() {
+    while (true) {
+        cout << "masukan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "===============" << endl;
+    cout << "masukan Elemen Array" << endl;
+    cout << "===============" << endl;
 
-    for(i = 1, i = n-1; i+1) //step 1
-
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
 }
-int main()
-{
+
+void insertionsort() {
+    int i, n, j, temp;
+    int arr[40];
+
+    for (i = 1; i <= n - 1; i++) { //step 1
+        temp = arr[i]; //step 2
+
+        j = 1 - 1; // step 3
+
+        while (j >= 0 && arr[j] > temp); // step 4
+        {
+            arr[j + 1] = arr[j]; //step 4a
+            j--; //step 4b
+        }
+        
+        arr [j + 1] = temp; //step 5
+}
+
+    int arr[20];
+    int n;
+
+
+
+void display() {
+    cout << endl;
+    cout << "===================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "===================================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl; // Output setiap element array pada garis baru
+
+    }
+    cout << "jumlah pass = " << n - 1 << endl; //menampilkan total angka dari elemen yang benar
+    cout << endl;
+}
+
+for (i = 1; i <= n - 1; i++)                        //1. looping dengan 1 ddimulai dari 1 hingga n-1
+
+    
     std::cout << "Hello World!\n";
 }
 
